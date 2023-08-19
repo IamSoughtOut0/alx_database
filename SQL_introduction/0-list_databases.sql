6ebe90038481d0130f1809f643e2986609e71dbf
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-19 02:43:33 +0100

    first database

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,21 @@
+--showing list
+
+SHOW databases;
+
+USE leonstore;
+
+SHOW tables;
+
+DESCRIBE buyers;
+
+
+
+--lsitng all my sql database--
+
+SHOW databases;
+
+USE leonstore;
+
+SHOW tables;
+
+DESCRIBE databases;
