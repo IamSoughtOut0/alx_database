24352149f32d34beb79eb030eda29b46dd540d4d
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-20 04:52:21 +0100

    Still updaing database

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,6 +2,10 @@
 
 USE mysql;
 
+CREATE DATABASE holbteron;
+
+USE holbteron;
+
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_2  (streetName varchar(50));
