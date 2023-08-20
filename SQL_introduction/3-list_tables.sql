5ead9a8c6293d3f1dbf8ea0959d1a9761529ca07
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-20 05:13:13 +0100

    Table Update

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,5 @@
 -- This is a single-line comment
-USE mysql;
+
 
 CREATE DATABASE IF NOT EXISTS Tables_in_hbtn_test_db_0;
 
@@ -8,6 +8,8 @@ USE Tables_in_hbtn_test_db_0;
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_2  (streetName varchar(50));
+
+USE mysql;
 
 SHOW TABLES;
 
