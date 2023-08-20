9dc542818d662cd4ee6c21aed7847ea4328aeb26
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-20 05:05:19 +0100

    Udating table in databse

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,9 @@
 -- This is a single-line comment
 USE mysql;
 
-CREATE DATABASE IF NOT EXISTS holbteron;
+CREATE DATABASE IF NOT EXISTS holbteronhbtn_test_db_0 ;
 
-USE holbteron;
+USE holbteronhbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
