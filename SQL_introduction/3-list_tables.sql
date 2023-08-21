49c12d20a6a1d09bccc6cdf62ef13b993986bcdb
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-21 05:19:36 +0100

    listing tables in database

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,9 @@
 -- This is a single-line comment
 USE mysql;
 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
-USE hbtn_0c_0;
+USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
