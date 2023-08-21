724aecaad19ba26f1a99d244cc2a337dd4ec401b
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-21 05:35:40 +0100

    lsinting tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -9,7 +9,7 @@ USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
-CREATE TABLE IF NOT EXISTS holbteron_2  (streetName varchar(50));
+CREATE TABLE IF NOT EXISTS holbteron_2 (streetName varchar(50));
 
 SHOW TABLES;
 
