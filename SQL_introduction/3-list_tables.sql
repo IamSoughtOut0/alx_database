bcd76be66610734c2e27e831d04602dc47131651
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-21 05:11:37 +0100

    creating tables in msql database

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,9 @@
 -- This is a single-line comment
-
-
-CREATE DATABASE IF NOT EXISTS mysql;
-
 USE mysql;
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+USE hbtn_0c_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
