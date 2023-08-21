3e5fac1ae0e7f09fe7183ab7b7ff086f461aa7ed
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-21 05:01:28 +0100

    listing table in msql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,15 +1,13 @@
 -- This is a single-line comment
 
 
-CREATE DATABASE IF NOT EXISTS Tables_in_hbtn_test_db_0;
+CREATE DATABASE IF NOT EXISTS mysql;
 
-USE Tables_in_hbtn_test_db_0;
+USE mysql;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_2  (streetName varchar(50));
-
-USE mysql;
 
 SHOW TABLES;
 
