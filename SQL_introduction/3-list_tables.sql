fd760f34d254b0a94b4039cca1fa3e094fe3b33e
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-22 13:56:12 +0100

    upading listing tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,11 +1,11 @@
 -- This is a single-line comment  
 CREATE DATABASE IF NOT EXISTS mysql; 
 
-USE IF EXISTS mysql; 
+USE mysql; 
 
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
-USE IF EXISTS hbtn_test_db_0;
+USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
