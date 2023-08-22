9fb12e4ef5fc73dfcf1e0e6f8b7a9f419d60f63e
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-22 14:22:46 +0100

    showing tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_2 (streetName varchar(50));
 
-SHOW TABLES;
+SHOW TABLES mysql;
 
