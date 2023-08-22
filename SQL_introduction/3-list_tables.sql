588fbb1a90bde16f7b26e7d175e40bc52209e1fe
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-22 14:01:37 +0100

    listing taable in mysql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -6,7 +6,7 @@ USE mysql;
 
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
---USE hbtn_test_db_0;
+-- USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
 CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
