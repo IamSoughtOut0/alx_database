3831423bf7375d1cfc37c56ceac58371c6a05da0
SoughtOutHiswill <70860773+SoughtOutHiswill@users.noreply.github.com>
2023-08-22 14:17:17 +0100

    listing tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -8,9 +8,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
 USE hbtn_test_db_0;
 
-CREATE TABLE IF NOT EXISTS holbteron_0 (firstName varchar(50));
-CREATE TABLE IF NOT EXISTS holbteron_1 (streetName varchar(50));
-CREATE TABLE IF NOT EXISTS holbteron_2 (streetName varchar(50));
+CREATE TABLE  holbteron_0 (firstName varchar(50));
+CREATE TABLE holbteron_1 (streetName varchar(50));
+CREATE TABLE holbteron_2 (streetName varchar(50));
 
 SHOW TABLES;
 
